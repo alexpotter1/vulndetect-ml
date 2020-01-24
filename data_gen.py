aeff50a38d7b10ed89b1f60ca31f036b0a28ca6d
Alex Potter <alexpotter1@icloud.com>
2020-01-24 12:42:06 +0000

    Unsparse arrays on load and adjust neural network structure
    Gets further into training, 1/2 epochs in then complains about the structure of the label tensor (cannot squeeze to 1 dimension), even though I'm using categorical_crossentropy which should accept one-hot encoded tensor data

--- data_gen.py
+++ data_gen.py
@@ -32,28 +32,36 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
             np.random.shuffle(self.indexes)
         
     def __data_generation(self, paths_temp):
-        x = np.empty((self.batch_size, *self.dim), dtype=object)
-        y = np.empty((self.batch_size), dtype=object)
+        x = []
+        y = []
 
-        for i, path in enumerate(paths_temp):
+        for _, path in enumerate(paths_temp):
             with np.load(path, allow_pickle=True) as data:
                 print("Loading %s" % path)
                 class_x = data['X']
                 print(class_x.shape)
                 class_y = data['Y']
 
-            if class_x.shape[0] != self.dim[0]:
-                continue
+            class_x = class_x[:self.dim[0]]
 
             # ensure data coming from file is uniform length
             min_csr_x_subshape_ind0 = min([x.shape[0] for x in class_x])
             min_csr_y_subshape_ind0 = min([y.shape[0] for y in class_y])
 
-            x_i_trim = np.asarray([i[:min_csr_x_subshape_ind0] for i in class_x], dtype=np.uint8)
-            print("trim_x shape: " + str(x_i_trim.shape))
-            x[i, ] = x_i_trim
+            x_i_trim = np.asarray([i[:min_csr_x_subshape_ind0].toarray() for i in class_x])
 
-            y[i] = np.asarray([i[:min_csr_y_subshape_ind0] for i in class_y], dtype=np.uint8)
+            '''if (x_i_trim.shape[1] < self.dim[1]):
+                diff = self.dim[1] - x_i_trim.shape[1]
+                npad = ((0,0), (0, diff), (0,0))
+                x_i_trim = np.pad(x_i_trim, pad_width=npad, mode='constant')'''
+
+            print("trim_x shape: " + str(x_i_trim.shape))
+            x.append(x_i_trim)
+
+            y.append(np.asarray([i[:min_csr_y_subshape_ind0].toarray() for i in class_y], dtype=np.uint8))
+
+        x = np.asarray(x)[0]
+        y = np.asarray(y)[0]
 
         # ensure x and y first dimension are equal lengths
         # pylint: disable=E1136
@@ -61,10 +69,10 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
             y = np.repeat(y, math.ceil(x.shape[0] / y.shape[0]))
             y = y[:x.shape[0]]
         elif (y.shape[0] > x.shape[0]):
-            y = y[x.shape[0]]
+            y = y[:x.shape[0]]
 
         # reshape to accommodate lstm timestep
-        x = np.reshape(x, (x.shape[0], 1, x.shape[1]))
+        # x = np.reshape(x, (x.shape[1], 1, x.shape[3]))
 
         print('X shape=%s' % str(x.shape))
         print('Y shape=%s' % str(y.shape))
--- simple_train.py
+++ simple_train.py
@@ -3,7 +3,7 @@
 import numpy as np
 import parse_training_input
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dropout, LSTM, Dense, Input
+from tensorflow.keras.layers import Dropout, LSTM, Dense, Input, Activation
 import util
 from data_gen import DataGenerator
 import time
@@ -26,7 +26,7 @@ p_dropout = (0.25, 0.5)
 hidden_dims = 128
 num_quantised_chars = len(util.supported_char_list)
 
-batch_size = 8
+batch_size = 1
 
 if not util.do_saved_vectors_exist():
     print("\nParsing training data...\n")
@@ -38,7 +38,7 @@ print("\nTraining...\n")
 # setup data loader (generator)
 labels = util.get_vulnerability_categories()
 generator_params = {
-    'dim': (17,),
+    'dim': (17, 300, 9506),
     'batch_size': batch_size,
     'n_classes': len(labels),
     'n_channels': 71,
@@ -62,14 +62,14 @@ vec_shuf = vectors[shuffle_indices]
 lab_shuf = labels[shuffle_indices]'''
 
 model = Sequential()
-
-model.add(Input(shape=(1, 17), sparse=True, batch_size=8))
-model.add(LSTM(256, input_shape=(1, 17), return_sequences=True))
+model.add(Input(shape=(None, 9506), sparse=False, batch_size=17))
+# model.add(Embedding(9506, 128, input_length=17))
+model.add(Dropout(0.4))
+model.add(LSTM(256, recurrent_dropout=0.2, dropout=0.2))
 model.add(Dropout(0.2))
-model.add(LSTM(256, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='softmax'))
-model.compile('adam', 'sparse_categorical_crossentropy', metrics=['accuracy'])
+model.add(Dense(97 * generator_params['n_classes']))
+model.add(Activation('softmax'))
+model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 print(model.summary())
 steps_per_epoch = ((len(labels) * util.MAX_FILE_PARSE) // batch_size)
 print("Steps/epoch: %s" % steps_per_epoch)
@@ -77,6 +77,6 @@ model.fit_generator(
     generator=training_gen,
     validation_data=validation_gen,
     steps_per_epoch=steps_per_epoch,
-    use_multiprocessing=True,
-    workers=12)
+    use_multiprocessing=False,
+    workers=1)
 model.save('save_temp.h5')
